5e7c3c013e238c7ee18cb1af257ed5ae7ead61dd
letstryitfirst <prathmeshsarwdnya@gmail.com>
2025-03-14 00:54:31 +0530

    feat: updating the packagejson

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const NavBar = () => {
     return (
         <nav>
-            <span className="logo"><a href="https://github.com/Ajinkyashinde1906" target='_blank'>AJ</a></span>
+            <span className="logo"><a href="https://github.com/letstryitfirst" target='_blank'>PS</a></span>
         </nav>
     )
 }
